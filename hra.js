9eb538a1ce3a36974a3f6cca96f0236f928bb35d
Katerina <kdatsenka@gmail.com>
2025-04-10 21:31:50 +0200

    add change player icon

--- hra.js
+++ hra.js
@@ -1,14 +1,17 @@
 let currentPlayer = 'circle';
 const gameBox = document.querySelectorAll('.game__box');
+const iconPlayer = document.querySelector('.game__icon--player');
 //console.log(buttons);
 
 const addPlayer = (event) => {
   if (currentPlayer === 'circle') {
     event.target.classList.add('board__field--circle');
     currentPlayer = 'cross';
+    iconPlayer.src = 'podklady/circle.svg';
   } else {
     event.target.classList.add('board__field--cross');
     currentPlayer = 'circle';
+    iconPlayer.src = 'podklady/cross.svg';
   }
 };
 
